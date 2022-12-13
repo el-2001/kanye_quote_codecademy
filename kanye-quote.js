abb46c7dc1e5119de1e2513d487cb3ff22e81610
el-2001 <eligla2001@outlook.com>
2022-12-12 18:48:20 -0600

    Added more pleasing visual to terminal output

--- kanye-quote.js
+++ kanye-quote.js
@@ -1,12 +1,12 @@
 
 import fetch from 'node-fetch';
 
-console.log('Your inspirational quote of the day by Kanye is...');
+console.log('Your inspirational quote of the day by Kanye is... \n ... \n ... ');
 const  getQuote = async () =>{
     const  quote = await fetch('https://api.kanye.rest').then(resolve =>{
     return resolve.json();
 }).then((response) =>{
-    return response.quote;
+    return `"${response.quote}" -Kanye`;
 })
 console.log(quote);
 }
